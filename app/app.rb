73456020d54fd2c48cdc2c449b07494ba0ceaa2b
Jay Bostock <jjnevis@gmail.com>
2018-03-16 14:14:59 +0000

    use javascript to encode the url again

--- app/app.rb
+++ app/app.rb
@@ -12,15 +12,15 @@ Document.ready? do
   Element.find('body').css('font-size', "#{@width/25}px")
 
   Element.find('#offNow').on :click do |event|
-    forward_url "sms:07860055401?body=52226 off 4"
+    forward_url 'sms:07860055401?body=52226%20off%204'
   end
 
   Element.find('#onNow').on :click do |event|
-    forward_url "sms:07860055401?body=52226 on 4"
+    forward_url 'sms:07860055401?body=52226%20on%204'
   end
 
   def forward_url url
-    `window.location = encodeURI(#{url})`
+    `window.location = #{url}`
   end
   # Element.find('td').on :click do |event|
   #   puts "The #{event.element.text} was clicked!"
