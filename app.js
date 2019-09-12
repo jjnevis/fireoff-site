147ef6c468be16940bec65e07068487d09f63254
Jay Bostock <jjnevis@gmail.com>
2019-09-12 18:39:39 +0100

    try cookies

--- app.js
+++ app.js
@@ -3145,14 +3145,43 @@ if (event == null) event = nil;
 if (event == null) event = nil;
     return self.$forward_url(event.$element().$text())}, TMP_7.$$s = self, TMP_7.$$arity = 1, TMP_7));
     
-    console.log("opal version is:");
-    console.log(Opal.const_get_relative($nesting, 'RUBY_ENGINE_VERSION'));
-  ;
+      function setCookie(cname,cvalue,exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+
+      function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
+
+      function checkCookie() {
+        var user=getCookie("username");
+        if (user != "") {
+          alert("Welcome again " + user);
+        } else {
+           user = prompt("Please enter your name:","");
+           if (user != "" && user != null) {
+             setCookie("username", user, 30);
+           }
+        }
+      }
+    ;
     
-    document.cookie = "username=John Doe";
-  ;
-    
-    console.log(document.cookie);
+    checkCookie;
     ;
     
     $writer = [getCookie("username");];
