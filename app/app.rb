e31754b86be45c9fa0e6d2128cbfdca194683918
Jay Bostock <jjnevis@gmail.com>
2019-09-27 16:05:53 +0100

    get from and to day working

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,5 @@
 require 'opal-jquery'
 require 'opal/jquery/local_storage'
-require 'text'
 
 ACTIVE_COLOUR = '#FFDD10'
 INACTIVE_COLOUR = '#DDD'
@@ -9,7 +8,19 @@ Document.ready? do
 
   @height = Document.height
   @width  = Document.width
-  @text = Text.new
+
+  time = Time.now
+  @time_from = time-time.sec - time.min%15*60 + 15*60 # next quarter hour
+
+  @day_from = @time_from.day
+  @hour_from = @time_from.hour
+  @min_from = @time_from.min
+
+  @time_to = @time_from+(60*60) # plus one hour
+
+  @day_to = @time_to.day
+  @hour_to = @time_to.hour
+  @min_to = @time_to.min
 
   @debug = Element.find('#debug_info')
 
@@ -24,66 +35,96 @@ Document.ready? do
   Element.find('tr').css('height', @height/16)
   Element.find('body').css('font-size', "#{@width/25}px")
 
+  @d_from = 0
+  if @day_to == @day_from
+    @d_to = 0
+  else
+    @d_to = 1
+  end
+
+  Element.find('#fd2').text =  (Time.now + 2*60*60*24).strftime("%A")
+  Element.find('#fd3').text =  (Time.now + 3*60*60*24).strftime("%A")
+
+  Element.find('#td2').text =  (Time.now + 2*60*60*24).strftime("%A")
+  Element.find('#td3').text =  (Time.now + 3*60*60*24).strftime("%A")
+
   Element.find('.quick').on :click do |event|
     forward_url event.element.text
-    @debug.text = @text.sms_message
   end
 
-  Element.find("#fh#{@text.hour_from}").css "background-color", ACTIVE_COLOUR
-  Element.find("#fm#{@text.min_from}").css "background-color", ACTIVE_COLOUR
-  Element.find("#th#{@text.hour_to}").css "background-color", ACTIVE_COLOUR
-  Element.find("#tm#{@text.min_to}").css "background-color", ACTIVE_COLOUR
+  Element.find("#fd0").css "background-color", ACTIVE_COLOUR
+  Element.find("#fh#{@hour_from}").css "background-color", ACTIVE_COLOUR
+  Element.find("#fm#{@min_from}").css "background-color", ACTIVE_COLOUR
 
-  Element.find('.fh').on :click do |event|
+  Element.find("#td0").css "background-color", ACTIVE_COLOUR
+  Element.find("#th#{@hour_to}").css "background-color", ACTIVE_COLOUR
+  Element.find("#tm#{@min_to}").css "background-color", ACTIVE_COLOUR
+
+  Element.find('.fd').on :click do |event|
+    Element.find("#fd#{@d_from}").css "background-color", INACTIVE_COLOUR
     event.element.css "background-color", ACTIVE_COLOUR
-    Element.find("#fh#{@text.hour_from}").css "background-color", INACTIVE_COLOUR
-    @text.hour_from = event.element.text
-    @debug.text = @text.sms_message
+    @d_from = event.element.id[2].to_i
+  end
+  Element.find('.fh').on :click do |event|
+    Element.find("#fh#{@hour_from}").css "background-color", INACTIVE_COLOUR
+    event.element.css "background-color", ACTIVE_COLOUR
+    @hour_from = event.element.text
   end
   Element.find('.fm').on :click do |event|
+    Element.find("#fm#{@min_from}").css "background-color", INACTIVE_COLOUR
     event.element.css "background-color", ACTIVE_COLOUR
-    Element.find("#fm#{@text.min_from}").css "background-color", INACTIVE_COLOUR
-    @text.min_from = event.element.text
-    @debug.text = @text.sms_message
+    @min_from = event.element.text
+  end
+  Element.find('.td').on :click do |event|
+    Element.find("#td#{@d_to}").css "background-color", INACTIVE_COLOUR
+    event.element.css "background-color", ACTIVE_COLOUR
+    @d_to = event.element.id[2].to_i
   end
   Element.find('.th').on :click do |event|
+    Element.find("#th#{@hour_to}").css "background-color", INACTIVE_COLOUR
     event.element.css "background-color", ACTIVE_COLOUR
-    Element.find("#th#{@text.hour_to}").css "background-color", INACTIVE_COLOUR
-    @text.hour_to = event.element.text
-    @debug.text = @text.sms_message
+    @hour_to = event.element.text
   end
   Element.find('.tm').on :click do |event|
+    Element.find("#tm#{@min_to}").css "background-color", INACTIVE_COLOUR
     event.element.css "background-color", ACTIVE_COLOUR
-    Element.find("#tm#{@text.min_to}").css "background-color", INACTIVE_COLOUR
-    @text.min_to = event.element.text
-    @debug.text = @text.sms_message
+    @min_to = event.element.text
   end
-  #   set_from_datepicker
-  #   set_to_datepicker
-  #   puts "clicked a fh!"
-  #   event.element.css "background-color", ACTIVE_COLOUR
-  # end
 
   Element.find('.book').on :click do |event|
     forward_url event.element.text
   end
 
   def forward_url code
-    update_static_data
+    if (get_from > get_to)
+      alert "FROM time is after TO time."
+    else
 
-    tel = Element.find('#tel').value
+      tel = LocalStorage['tel'] = Element.find('#tel').value
+      pin = LocalStorage['pin'] = Element.find('#pin').value
 
-    @text.pin_code = Element.find('#pin').value
+      if ['On', 'Of'].include? code[0,2]
+        url = "sms:#{tel}?body=#{pin} #{code}"
+      else
+        url = "sms:#{tel}?body=#{pin} #{sms_message} #{code}"
+      end
 
-    @text.code = code
-    url = "sms:#{tel}?body=#{@text.sms_message}"
-
-    `window.location = encodeURI(#{url})`
+      `window.location = encodeURI(#{url})`
+    end
   end
 
-  def update_static_data
-    LocalStorage['tel'] = Element.find('#tel').value
-    LocalStorage['pin'] = Element.find('#pin').value
+  def sms_message
+    "#{get_from.strftime("%d%m%y")} #{get_from.strftime("%H%M")} #{get_to.strftime("%d%m%y")} #{get_to.strftime("%H%M")}"
+  end
+
+  def get_from
+    ft = (@time_from + (@d_from*24*60*60))
+    Time.new(ft.year, ft.month, ft.day, @hour_from, @min_from)
+  end
+
+  def get_to
+    ft = (@time_from + (@d_to*24*60*60))
+    Time.new(ft.year, ft.month, ft.day, @hour_to, @min_to)
   end
 
 end
